1aa0c6b1dae4ef9e49fc76f8fa55f8efbf12ddbc
atmoose <icamoose@gmail.com>
2022-09-12 10:41:26 -0400

    Add files via upload
    Limited the number of loans being handled

--- ui.R
+++ ui.R
@@ -4,7 +4,7 @@ ui <- fluidPage(
   theme = bslib::bs_theme(version = 5, bootswatch = "solar"),
   sidebarLayout(
     sidebarPanel(
-      numericInput("n", "Number of loans", value = 1, min = 1),
+      numericInput("n", "Number of loans", value = 1, min = 1, max = 8),
       selectInput("method", "Method", choices = c("Avalanche", "Snowball")),
       radioButtons("know", "Do you know your monthly payments?", c("Yes", "No")),
       numericInput("amt", "Total payment to be split among loans", value = 50, min = 1),
@@ -21,7 +21,10 @@ ui <- fluidPage(
                            p("This app only works for loans with monthly payments. If you don't know your monthly
                              payments for each loan, then the app's calculated values might differ slightly from
                              your actual values. Your loan holders might do intermediate rounding when calcuating
-                             your loan payments which this app does not."),
+                             your loan payments which this app does not do. If you've already paid off some of a
+                             loan and don't know your monthly payment, you can calculate the monthly payment using
+                             the initial values for the loan. Then you can use the calculated payment with the
+                             current loan balance."),
                            h3("Inputs"),
                            p("Select the number of loans you want to include and how much each month you can
                              afford to pay towards those loans. Fill in the generated table on the next
